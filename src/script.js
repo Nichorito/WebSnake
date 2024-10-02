f24d1f25ffee65009eeeb1e4131f87a1b63deeb5
Nichorito <nsheppard999@gmail.com>
2024-10-02 14:22:02 -0400

    Create grid and title, plus basic styling

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
 import "./styles.css";
+
+
+
+//////////////////////
+//// UI ELEMENTS /////
+//////////////////////
+
+const gridContainer = document.getElementById('grid');
+        for (let row = 0; row < 20; row++) {
+            for (let col = 0; col < 40; col++) {
+                let cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.setAttribute('data-row', row);
+                cell.setAttribute('data-col', col);
+                cell.innerText = `cell[${row}][${col}]`;
+                gridContainer.appendChild(cell);
+            }
+        }
